1c04d77f5c59329719436f5c20fe4d18434625e0
node <18511697672@qq.com>
2017-11-15 13:57:18 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,86 +1,64 @@
+var convert = require('xml-js');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+var fio = require('./lib/wx/fileio');
 
-var wxrequest = require('./lib/wx/wxrequest');
-var  convert = require('xml-js');
-var  process = require('process');
-var  fio = require('./lib/wx/fileio');
+var xml = fio.readConfig();
+var xjson = JSON.parse( convert.xml2json(xml, {
+	compact: true,
+	spaces: 4
+}) );
 
-
-var main = function() {
-
-	var xml = fio.readConfig();
-	var xjson = JSON.parse( convert.xml2json(xml, {
-		compact: true,
-		spaces: 4
-	}) );
-	
 	process.env.appid= xjson.xml.appid._text;
 	process.env.appsecret= xjson.xml.appsecret._text;
 	process.env.access_token= xjson.xml.access_token._text;
 	process.env.expires_in= xjson.xml.expires_in._text;
 	process.env.token =  xjson.xml.token._text;
-	process.env.wxpubacc = xjson.xml.wxpubacc._text;	
-/**
-	setImmediate(function(){
-		wxrequest.getAuthToken(process.env.appid, process.env.appsecret, function(d) {
-			if(d && d.access_token){
+	process.env.wxpubacc = xjson.xml.wxpubacc._text;
 
-				process.env.access_token= d.access_token;
-				var access_token = d.access_token;
-				console.log(d.access_token +'--'+ access_token.length);
-				fio.saveAccessToken(d.access_token);
+	/*创建工作进程*/
+	console.log('master '+ process.pid +' is running');
+	if(numCPUs == 1) {
+		/*单核-主进程监听2000，子进程监听80*/
+		require('./bin/www');
+		cluster.setupMaster({
+				exec: 'index.js',
+				args: ['--use','http', 80],
+				silent: true
+		});	
+		cluster.fork();
+	}else {
+		for(var  i = 0; i< numCPUs; i++) {
+			/*多核-1个cpu分配给80，其他分配给2000*/
+			if(i == numCPUs-1) {
+				cluster.setupMaster({
+					exec: 'index.js',
+					args: ['--use','http', 80],
+					silent: true
+				});		
+			}else {
+				cluster.setupMaster({
+					exec: 'bin/www',
+					args: ['--use', 'http', 2000],
+					silent: true
+				});
 			}
-		});
-	});
-**/
-/**
-	setInterval(function(){
-		wxrequest.getAuthToken(process.env.appid, process.env.appsecret, function(d) {
-			if(d && d.access_token){
-
-				process.env.access_token= d.access_token;
-				fio.saveAccessToken(d.access_token);
-			}
-		});
-	}, 6000*1000);
-**/
-/**
-	var rl = readline.createInterface({
-		input: fs.createReadStream('./xml/appinfo.xml'),
-	});
-	rl.on('line', (line)=> {
-		console.log('---'+line +'\n');
-		var n;
-		if((n= line.indexOf('</access_token>'))!= -1) {
-					
+			cluster.fork();
 		}
-	});
-**/
-/**
-	wxrequest.getAuthToken(appinfo_test.id, appinfo_test.secret, function(d) {
-		if(d){
-			appinfo_test['token'] = d.access_token;
-			//wxconfig_api.app.init(appinfo_test);
-			console.log(d.access_token);
-			var menu = {
-				'button': [
-					{
-						'type': 'view',
-						'name': '我要',
-						'url': 'http://baidu.com'
-					}
-				]
-			};
-			wxrequest.createMenu(d.access_token, menu, function(d) {
-				console.log(JSON.stringify(d));
-				if(errcode == 0){
-					console.log('----菜单创建成功');
-				}else {
-					console.log('error ---'+d.errmsg);
-				}
-			});
-		}
+	}
 	
+	cluster.on('exit', (worker, code, signal) => {
+		console.log('worker '+ worker.process.pid +' exit --code '+code);
 	});
-**/
-};
-module.exports = main;
+
+	/*进程之间通讯,默认限制10个事件*/
+		
+	//process.send({cmd: 'notify', 'pid': process.pid, 'args': process.argv.splice(0)});  //子进程给住进程发送消息
+	var messageHandler = function(msg) {
+		if(msg.cmd && msg.cmd === 'notify') {
+			console.log('worker pid:'+msg.pid+' args--'+msg.args)
+		}
+	}
+	for(var id in cluster.workers) {
+		cluster.workers[id].on('message', messageHandler);
+	}
